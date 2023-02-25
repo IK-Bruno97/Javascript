311bf80f97019114d3aee30b39510a60a2c36bc0
Ikenna <69594989+IK-Bruno97@users.noreply.github.com>
2023-02-25 17:02:34 +0100

    Add files via upload

---
+++ fibbo.js
@@ -0,0 +1,12 @@
+<script>
+  let fibby = function (count) {
+    console.log(`Fibby: ${count}`); 
+    if (count === 1) return [0, 1]; 
+    let arr = fibby(count-1); 
+    let sum = arr[arr.length - 1] + arr[arr.length - 2]; 
+    arr.push(sum); 
+    console.log(arr); 
+    return arr;
+  }; 
+  console.log(fibby(8))
+</script>
